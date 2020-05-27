ddfb618f488cc095b3f8b30aeea242cb887d5eec
Mikle Savchenko <svampx@gmail.com>
2020-05-27 20:55:41 +0300

    deploying with heroku constraunts

---
+++ src/test/resources/db.changelog/books.sql
@@ -0,0 +1,13 @@
+INSERT INTO library.books
+values (default, 'The Adventures of Sherlock Holmes', 'Arthur Conan Doyle', 'DETECTIVE_FICTION', 1892, false, 5, '2020-05-21 17:13:46.307', '2020-05-21 17:13:46.307', 'ACTIVE'),
+(default,	'TheTo Kill a Mockingbird',	'Harper Lee',	'HISTORICAL_FICTION',	1960,	false,	8,	'2020-05-21 17:18:02.239',	'2020-05-21 17:18:02.239',	'ACTIVE'),
+(default,	'1984',	'George Orwell',    'SCIENCE_FICTION',	1950,	false,	9,	'2020-05-21 17:19:17.665',	'2020-05-21 17:19:17.665',	'ACTIVE'),
+(default,	'The Lord of the Rings',	'J.R.R. Tolkien',	'FANTASY',	1950,	false,	5,	'2020-05-21 17:22:23.494',	'2020-05-21 17:22:23.494',	'ACTIVE'),
+(default,	'The Lion, the Witch and the Wardrobe',	'C.S. Lewis',	'FANTASY',	2005,	false,	4,	'2020-05-21 17:27:27.525',	'2020-05-21 17:27:27.525',	'ACTIVE'),
+(default,	'TheA Clockwork Orange',	'Anthony Burgess',	'SCIENCE_FICTION',	2005,	false,	6,	'2020-05-21 17:29:18.829',	'2020-05-21 17:29:18.829',	'ACTIVE'),
+(default,	'Alice’s Adventures In Wonderland','Lewis Carroll',	'FANTASY',	2004,	false,	7,	'2020-05-21 17:32:37.974',	'2020-05-21 17:32:37.974',	'ACTIVE'),
+(default, 'Empire of the Ants', 'Bernard Werber', 'NOVEL', 1991, true, 5, '2020-05-21 17:22:23.494', '2020-05-21 17:22:23.494', 'ACTIVE'),
+(default, 'Fight Club', 'Chuck Palahniuk', 'NOVEL', 1996, true, 7, '2020-05-21 17:18:02.239', '2020-05-21 17:18:02.239', 'ACTIVE'),
+(default, 'The Plague', 'Albert Camus', 'NOVEl', 1988, true, 4, '2020-05-21 17:13:46.307', '2020-05-21 17:13:46.307', 'ACTIVE'),
+(default,'Harry Potter and the Philosopher''s Stone', 'J. K. Rowling', 'FANTASY', 1999, true, 5, '2020-05-21 17:27:27.525',	'2020-05-21 17:27:27.525', 'ACTIVE'),
+(default, 'Fantastic Beasts and Where to Find Them', 'J. K. Rowling', 'FANTASY', 2012, true, 5, '2020-05-21 17:27:27.525',	'2020-05-21 17:27:27.525', 'ACTIVE');
