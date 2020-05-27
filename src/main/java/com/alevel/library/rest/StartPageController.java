6971068461cbd01e5877770bb88af65347e019c7
Mikle Savchenko <svampx@gmail.com>
2020-05-27 22:15:53 +0300

    Ok, now simple hello page added

--- src/main/java/com/alevel/library/rest/StartPageController.java
+++ src/main/java/com/alevel/library/rest/StartPageController.java
@@ -1,4 +1,16 @@
 package com.alevel.library.rest;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
 public class StartPageController {
+
+    @GetMapping
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("Hello from Heroku A-level-library app!");
+    }
 }
